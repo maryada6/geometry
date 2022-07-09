fe22518e887bb185445130f475074f124dbb6500
Maryada Kumar Lodha <maryadakumar@SUI-C0042-X0.local>
2022-07-09 08:58:47 +0530

    Ensure point coordinates are not initialised with string values

--- lib/geometry/point.rb
+++ lib/geometry/point.rb
@@ -1,5 +1,9 @@
 module Geometry
   class Point
-    def initialize(x_coordinate, y_coordinate) end
+    def initialize(x_coordinate, y_coordinate)
+      if x_coordinate.is_a?(String) or y_coordinate.is_a?(String)
+        raise "Coordinates cannot be string values"
+      end
+    end
   end
 end
--- spec/geometry/point_spec.rb
+++ spec/geometry/point_spec.rb
@@ -3,5 +3,9 @@ RSpec.describe Geometry::Point do
     it 'should be initialised with two coordinates x,y' do
       expect { Geometry::Point.new(1, 2) }.not_to raise_error
     end
+
+    it 'should handle case when both x and y coordinates are string' do
+      expect { Geometry::Point.new("2", "3") }.to raise_error("Coordinates cannot be string values")
+    end
   end
 end
