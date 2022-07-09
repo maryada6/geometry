963507baa3d454ad32a1162405427ae545b94a43
Maryada Kumar Lodha <maryadakumar@SUI-C0042-X0.local>
2022-07-09 08:16:10 +0530

    Initialize project

---
+++ lib/geometry.rb
@@ -0,0 +1,6 @@
+require "geometry/version"
+
+module Geometry
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/geometry_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Geometry do
+  it "has a version number" do
+    expect(Geometry::VERSION).not_to be nil
+  end
+end
