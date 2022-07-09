9014814e6b61c04193214b873424086861c4f489
Maryada Kumar Lodha <maryadakumar@SUI-C0042-X0.local>
2022-07-09 09:13:51 +0530

    Ensure point coordinates are not initialised with null value

--- lib/geometry/point.rb
+++ lib/geometry/point.rb
@@ -1,9 +1,18 @@
 module Geometry
   class Point
+    attr :x_coordinate, :y_coordinate
+
     def initialize(x_coordinate, y_coordinate)
+      @x_coordinate = x_coordinate
+      @y_coordinate = y_coordinate
       if x_coordinate.is_a?(String) or y_coordinate.is_a?(String)
         raise "Coordinates cannot be string values"
       end
+
+      if x_coordinate.nil? or y_coordinate.nil?
+        raise "Coordinates cannot be nil values"
+      end
+
     end
   end
 end
--- spec/geometry/point_spec.rb
+++ spec/geometry/point_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Geometry::Point do
     it 'should handle case when both x and y coordinates are string' do
       expect { Geometry::Point.new("2", "3") }.to raise_error("Coordinates cannot be string values")
     end
+
+    it 'should handle case when x or y coordinates are nil' do
+      expect { Geometry::Point.new(nil, nil) }.to raise_exception("Coordinates cannot be nil values")
+    end
+
   end
 end
